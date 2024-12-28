0cef78938fce7e045a7d4cc7d1f5543279a34db8
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2024-12-28 08:24:28 +0530

    data

--- backend/server.mjs
+++ backend/server.mjs
@@ -45,6 +45,9 @@ const VehicleSchema = new mongoose.Schema({
     rcownername: { type: String },
     dealername: { type: String },
     dealerphone: { type: String },
+    costprice:{type:String},
+    spent:{type:String},
+    sellingprice:{type:String},
     rcbookfile: { type: String },
     aadharbook: { type: String },
     vehiclephoto: { type: String },
@@ -123,29 +126,6 @@ app.post(
     }
 );
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ error: 'No file uploaded' });
-        }
-
-        // Construct the file path URL for accessing the uploaded file
-        const filePath = `https://star-autos.onrender.com/uploads/${req.file.filename}`;
-
-        res.status(200).json({
-            message: 'File uploaded successfully',
-            file: {
-                name: req.file.filename,
-                type: req.file.mimetype,
-                size: req.file.size,
-                path: filePath,
-            },
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
 app.get('/api/vehicledata', async (req, res) => {
     try {
         const vehicleData = await Vehicle.find(); // Fetch all vehicle data
@@ -198,7 +178,7 @@ app.get('/api/server/getLatestAc', async (req, res) => {
 
 app.get('/api/server/search-vehicle', async (req, res) => {
     try {
-        const { hp, registernumber } = req.query;
+        const { hp, registernumber } = req.query; 
 
         const query = {};
 
@@ -206,7 +186,7 @@ app.get('/api/server/search-vehicle', async (req, res) => {
             query.acNumber = hp;
         }
         if (registernumber) {
-            query.registernumber = registernumber;
+            query.registernumber = registernumber; 
         }
 
         const vehicle = await Vehicle.find(query);
@@ -258,7 +238,7 @@ app.put(
             }
 
             const updatedVehicle = await Vehicle.findOneAndUpdate(
-                { acNumber: hp },
+                { acNumber:hp },
                 updatedData,
                 { new: true }
             );
